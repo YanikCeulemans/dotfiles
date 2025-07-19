5f3a0008a959c72d2d84a753a5789c571af2e037
Yanik Ceulemans <yanik.ceulemans@gmail.com>
2025-07-19 16:45:39 +0200

    frappe iso macchiato fuzzel

--- dot_config/yanikc/executable_toggle-dark-mode.sh
+++ dot_config/yanikc/executable_toggle-dark-mode.sh
@@ -2,13 +2,11 @@
 set -euo
 
 if test "$(gsettings get org.gnome.desktop.interface color-scheme)" = "'prefer-light'"; then
-  sed -i 's/catppuccin-latte/catppuccin-macchiato/' ~/.config/alacritty/alacritty.toml
-  sed -i 's/catppuccin-latte/catppuccin-macchiato/' ~/.config/fuzzel/fuzzel.ini
+  sed -i 's/catppuccin-latte/catppuccin-frappe/' ~/.config/fuzzel/fuzzel.ini
   gsettings set org.gnome.desktop.interface color-scheme prefer-dark
   gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 else
-  sed -i 's/catppuccin-macchiato/catppuccin-latte/' ~/.config/alacritty/alacritty.toml
-  sed -i 's/catppuccin-macchiato/catppuccin-latte/' ~/.config/fuzzel/fuzzel.ini
+  sed -i 's/catppuccin-frappe/catppuccin-latte/' ~/.config/fuzzel/fuzzel.ini
   gsettings set org.gnome.desktop.interface color-scheme prefer-light
   gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
 fi
